78fd8521da9b069486cd26627f3c6d4582cf6fd4
Nicolas Tambosco <tambosconicolas123@gmail.com>
2023-11-28 10:16:37 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import {
-    BrowserRouter as Router,
-    Navigate,
-    Route,
-    Routes,
-  } from "react-router-dom";
-  
-  import Login from "./pages/Login";
-  import Pelicula from "./pages/Pelicula";
-  import Verificacion  from "./pages/Verificacion";
-  import ForgotPassword from './pages/ForgotPassword';
-  import Register from './pages/Register';
-  export default function App() {
-    return (
-      <Router>
-        
-  
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Pelicula" element={<Pelicula />} />
-          <Route path="/Verificacion/*" element={<Verificacion />}>
-            <Route path="welcome" element={<p>Welcome!</p>} />
-          </Route>
-          
-        </Routes>
-      </Router>
-    );
-  }
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Pelicula from "./pages/Pelicula";
+import Verificacion from "./pages/Verificacion";
+import ForgotPassword from './pages/ForgotPassword';
+import Register from './pages/Register';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Ruta de la página de inicio */}
+        <Route path="/" element={<Navigate to="/Login" />} />
+
+        {/* Resto de las rutas */}
+        <Route path="/Login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Pelicula" element={<Pelicula />} />
+        <Route path="/Verificacion/*" element={<Verificacion />}>
+          <Route path="welcome" element={<p>Welcome!</p>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
